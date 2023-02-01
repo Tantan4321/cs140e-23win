96800bcb02a672be2e612ea7e8b90e49c906f5e9
Eastan Giebler <eastan@stanford.edu>
2023-01-31 17:04:02 -0800

    check in

--- labs/4-cross-checking/00-hello/test-interfile.d
+++
@@ -1,6 +0,0 @@
-test-interfile.o: test-interfile.c \
- /home/eastan/Desktop/cs140e-23win/libpi/include/rpi.h \
- /home/eastan/Desktop/cs140e-23win/libpi/libc/demand.h \
- /home/eastan/Desktop/cs140e-23win/libpi/include/gpio.h \
- /home/eastan/Desktop/cs140e-23win/libpi/include/rpi-constants.h \
- /home/eastan/Desktop/cs140e-23win/libpi/include/your-prototypes.h foo.h
