ddcd3274f7095aa113ab50d10a0f0b5d80948190
Eastan Giebler <eastan@stanford.edu>
2023-01-31 18:07:02 -0800

    Read file prelab 7

--- libunix/read-file.c
+++ libunix/read-file.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "libunix.h"
 
@@ -25,6 +26,40 @@ void *read_file(unsigned *size, const char *name) {
     //    - zero pads to a multiple of 4.
     //    - read entire file into buffer.  
     //    - make sure any padding bytes have zeros.
-    //    - return it.   
-    unimplemented();
+    //    - return it.
+    struct stat *sstat;
+    sstat = malloc(sizeof(struct stat));
+    if (sstat == NULL) {
+        printf("fatal error: stat() memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    stat(name, sstat);
+    *size = sstat->st_size;
+    free(sstat);
+
+    FILE *file = fopen(name, "r");
+    if (file == NULL) {
+        printf("fatal error: open/read of %s fails\n",
+               name);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned rounded = (*size | 0x03) + 1;
+
+    char *buf = (char *)calloc(rounded, sizeof(char));
+    if (buf == NULL) {
+        printf("fatal error: read buffer memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_read = read(fileno(file), buf, *size);
+    if (*size != (unsigned)bytes_read) {
+        printf("fatal error: read failed, wanted to read %d bytes but only read %d\n",
+               *size, (unsigned)bytes_read);
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(file);
+    return (void *)buf;
 }
