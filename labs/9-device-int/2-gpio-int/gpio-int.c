15d54b241f0313d721fce63105ea4f7d36154750
Eastan Giebler <eastan@stanford.edu>
2023-02-07 20:45:01 -0800

    Lab 9 Part 2

--- labs/9-device-int/2-gpio-int/gpio-int.c
+++ labs/9-device-int/2-gpio-int/gpio-int.c
@@ -1,5 +1,31 @@
 // engler, cs140 put your gpio-int implementations in here.
 #include "rpi.h"
+#include "rpi-interrupts.h"
+#include "gpio.h"
+
+enum {
+    GPIO_BASE = 0x20200000,
+    gpio_set0  = (GPIO_BASE + 0x1C),
+    gpio_clr0  = (GPIO_BASE + 0x28),
+    gpio_lev0  = (GPIO_BASE + 0x34),
+    gpio_pud = (GPIO_BASE + 0x94),
+    gpio_pudclk0 = (GPIO_BASE + 0x98),
+    GPIO_GPREN0 = (GPIO_BASE + 0x4c),
+    GPIO_GPFEN0 = (GPIO_BASE + 0x58),
+    GPIO_GPEDS0 = (GPIO_BASE + 0x40)
+};
+
+
+static void OR32(uint32_t addr, uint32_t val) {
+    dev_barrier();
+    PUT32(addr, GET32(addr) | val);
+    dev_barrier();
+}
+
+
+void set_gpio_int(unsigned gpio_int) {
+    PUT32(Enable_IRQs_2, (1 << (gpio_int - 32)));
+}
 
 // returns 1 if there is currently a GPIO_INT0 interrupt, 
 // 0 otherwise.
@@ -7,7 +33,9 @@
 // note: we can only get interrupts for <GPIO_INT0> since the
 // (the other pins are inaccessible for external devices).
 int gpio_has_interrupt(void) {
-    todo("implement: is there a GPIO_INT0 interrupt?\n");
+    unsigned val = GET32(IRQ_pending_2) & (1 << (GPIO_INT0 - 32));
+    if (val > 0) return DEV_VAL32(1);
+    else return DEV_VAL32(0);
 }
 
 // p97 set to detect rising edge (0->1) on <pin>.
@@ -16,26 +44,40 @@ int gpio_has_interrupt(void) {
 // *after* a 1 reading has been sampled twice, so there will be delay.
 // if you want lower latency, you should us async rising edge (p99)
 void gpio_int_rising_edge(unsigned pin) {
-    todo("implement: detect rising edge\n");
+    if(pin >= 32) return;
+    OR32(GPIO_GPREN0, (1 << pin));
+    set_gpio_int(GPIO_INT0);
+    dev_barrier();
 }
 
-// p98: detect falling edge (1->0).  sampled using the system clock.  
+// p98: detect falling edge (1->0).  sampled using the system clock.
 // similarly to rising edge detection, it suppresses noise by looking for
-// "100" --- i.e., is triggered after two readings of "0" and so the 
+// "100" --- i.e., is triggered after two readings of "0" and so the
 // interrupt is delayed two clock cycles.   if you want  lower latency,
 // you should use async falling edge. (p99)
 void gpio_int_falling_edge(unsigned pin) {
-    todo("implement: detect falling edge\n");
+    if(pin >= 32) return;
+    OR32(GPIO_GPFEN0, (1 << pin));
+    set_gpio_int(GPIO_INT0);
+    dev_barrier();
 }
 
 // p96: a 1<<pin is set in EVENT_DETECT if <pin> triggered an interrupt.
-// if you configure multiple events to lead to interrupts, you will have to 
+// if you configure multiple events to lead to interrupts, you will have to
 // read the pin to determine which caused it.
 int gpio_event_detected(unsigned pin) {
-    todo("implement: is an event detected?\n");
+    if(pin >= 32) return DEV_VAL32(0);
+    dev_barrier();
+    unsigned event = GET32(GPIO_GPEDS0) & (1<<pin);
+    dev_barrier();
+    if (event > 0) return DEV_VAL32(1);
+    else return DEV_VAL32(0);
 }
 
 // p96: have to write a 1 to the pin to clear the event.
 void gpio_event_clear(unsigned pin) {
-    todo("implement: clear event on <pin>\n");
+    if(pin >= 32) return;
+    dev_barrier();
+    PUT32(GPIO_GPEDS0, (1<<pin));
+    dev_barrier();
 }
--- libpi/src/gpio.c
+++ libpi/src/gpio.c
@@ -8,6 +8,7 @@
  * See rpi.h in this directory for the definitions.
  */
 #include "rpi.h"
+#include "rpi-interrupts.h"
 
 // see broadcomm documents for magic addresses.
 enum {
@@ -16,12 +17,17 @@ enum {
     gpio_clr0  = (GPIO_BASE + 0x28),
     gpio_lev0  = (GPIO_BASE + 0x34),
     gpio_pud = (GPIO_BASE + 0x94),
-    gpio_pudclk0 = (GPIO_BASE + 0x98)
+    gpio_pudclk0 = (GPIO_BASE + 0x98),
+    GPIO_GPREN0 = (GPIO_BASE + 0x4c),
+    GPIO_GPFEN0 = (GPIO_BASE + 0x58),
+    GPIO_GPEDS0 = (GPIO_BASE + 0x40)
 };
 
-//
-// Part 1 implement gpio_set_on, gpio_set_off, gpio_set_output
-//
+static void OR32(uint32_t addr, uint32_t val) {
+    dev_barrier();
+    PUT32(addr, GET32(addr) | val);
+    dev_barrier();
+}
 
 // set <pin> to be an output pin.
 //
@@ -136,4 +142,64 @@ void gpio_set_function(unsigned pin, gpio_func_t func) {
     value &= ~mask;
     value |= (func << (3 * (pin % 10)));
     PUT32(gpio_fsel, value);
+}
+
+
+void set_gpio_int(unsigned gpio_int) {
+    PUT32(Enable_IRQs_2, (1 << (gpio_int - 32)));
+}
+
+// returns 1 if there is currently a GPIO_INT0 interrupt,
+// 0 otherwise.
+//
+// note: we can only get interrupts for <GPIO_INT0> since the
+// (the other pins are inaccessible for external devices).
+int gpio_has_interrupt(void) {
+    unsigned val = GET32(IRQ_pending_2) & (1 << (GPIO_INT0 - 32));
+    if (val > 0) return DEV_VAL32(1);
+    else return DEV_VAL32(0);
+}
+
+// p97 set to detect rising edge (0->1) on <pin>.
+// as the broadcom doc states, it  detects by sampling based on the clock.
+// it looks for "011" (low, hi, hi) to suppress noise.  i.e., its triggered only
+// *after* a 1 reading has been sampled twice, so there will be delay.
+// if you want lower latency, you should us async rising edge (p99)
+void gpio_int_rising_edge(unsigned pin) {
+    if(pin >= 32) return;
+    OR32(GPIO_GPREN0, (1 << pin));
+    set_gpio_int(GPIO_INT0);
+    dev_barrier();
+}
+
+// p98: detect falling edge (1->0).  sampled using the system clock.
+// similarly to rising edge detection, it suppresses noise by looking for
+// "100" --- i.e., is triggered after two readings of "0" and so the
+// interrupt is delayed two clock cycles.   if you want  lower latency,
+// you should use async falling edge. (p99)
+void gpio_int_falling_edge(unsigned pin) {
+    if(pin >= 32) return;
+    OR32(GPIO_GPFEN0, (1 << pin));
+    set_gpio_int(GPIO_INT0);
+    dev_barrier();
+}
+
+// p96: a 1<<pin is set in EVENT_DETECT if <pin> triggered an interrupt.
+// if you configure multiple events to lead to interrupts, you will have to
+// read the pin to determine which caused it.
+int gpio_event_detected(unsigned pin) {
+    if(pin >= 32) return DEV_VAL32(0);
+    dev_barrier();
+    unsigned event = GET32(GPIO_GPEDS0) & (1<<pin);
+    dev_barrier();
+    if (event > 0) return DEV_VAL32(1);
+    else return DEV_VAL32(0);
+}
+
+// p96: have to write a 1 to the pin to clear the event.
+void gpio_event_clear(unsigned pin) {
+    if(pin >= 32) return;
+    dev_barrier();
+    PUT32(GPIO_GPEDS0, (1<<pin));
+    dev_barrier();
 }
